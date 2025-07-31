22488c72c3b6f61155fba7b480c0b8baab6c6418
Samuel-Leal96 <saam_lv96@hotmail.com>
2025-07-31 09:31:40 -0600

    Creando el state del carrito
    Se manda el state al componente de guitar por medio de prop y se agregan al estado las guitarras por medio de un click

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import { useEffect, useState } from "react";
 
 function App() {
 
-  const [data, setData] = useState(db)
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
 
   return (
     <>
@@ -21,6 +22,7 @@ function App() {
             <Guitar
               key={guitar.id}
               guitar={guitar}
+              setCart={setCart}
             />
           ))}
 
